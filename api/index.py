6b9650fb5496d0706bf930e8e14b6916f20ebc02
Zfour <499984532@qq.com>
2021-03-02 00:08:23 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -27,5 +27,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(json.dumps(data).encode())
+        self.wfile.write(json.dumps(data).encode('utf-8'))
         return
