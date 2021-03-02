12161a668849ef2281f9af533ee11c1fd4d09d55
Zfour <499984532@qq.com>
2021-03-02 23:47:59 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,11 @@
 # -*- coding: UTF-8 -*-
 import requests
-import re
 import json
 from http.server import BaseHTTPRequestHandler
 from bs4 import BeautifulSoup
 
-def github_json(user,repo):
-    requests_path = 'https://github.com/' + user + '/' +repo + '/blob/master/friendlist.json'
+def github_json(user,repo,branch):
+    requests_path = 'https://github.com/' + user + '/' +repo + '/blob/'+branch+'/friendlist.json'
     r = requests.get(requests_path)
     r.encoding = 'utf-8'
     gitpage = r.text
@@ -15,15 +14,34 @@ def github_json(user,repo):
     result = json.loads(main_content)
     return result
 
+def url_split(url):
+    info_list = []
+    url_text_list = url.split('/')
+    if len(url_text_list) == 7:
+        info_list.append(url_text_list[4])
+        info_list.append(url_text_list[5])
+        info_list.append(url_text_list[6])
+    if len(url_text_list) == 6:
+        info_list.append(url_text_list[4])
+        info_list.append(url_text_list[5])
+        info_list.append('master')
+    if len(url_text_list) == 5:
+        info_list.append(url_text_list[4])
+        info_list.append('friends')
+        info_list.append('master')
+    return info_list
+
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         path = self.path
-        path = path.replace("'", '"')
-        repo_reg = re.compile(r'repo="(.*?)"')
-        user_reg = re.compile(r'user="(.*?)"')
-        user = user_reg.findall(path)[0]
-        repo = repo_reg.findall(path)[0]
-        data = github_json(user,repo)
+        info_list = url_split(url)
+        try:
+            user = info_list[0]
+            repo = info_list[1]
+            branch = info_list[2]
+            data = github_json(user,repo,branch)
+        except:
+            data =['message':'error']
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type', 'application/json')
