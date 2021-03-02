a06871a05dfd23dfbf9e75d290e0d6a6ec582d51
Zfour <499984532@qq.com>
2021-03-03 00:10:36 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,29 +6,36 @@ from http.server import BaseHTTPRequestHandler
 from bs4 import BeautifulSoup
 
 def github_json(user,repo,branch):
-    requests_path = 'https://github.com/' + user + '/' +repo + '/blob/'+branch+'/friendlist.json'
-    r = requests.get(requests_path)
-    r.encoding = 'utf-8'
-    gitpage = r.text
-    soup = BeautifulSoup(gitpage, 'html.parser')
-    main_content = soup.find('td',id = 'LC1').text
-    result = json.loads(main_content)
+    if user =='':
+        result = json.loads("['error':'用户为空']")
+    else:
+        try:
+            if repo =='':
+                repo = 'friends'
+            if branch ==''
+                branch = 'master'
+            requests_path = 'https://github.com/' + user + '/' +repo + '/blob/'+branch+'/friendlist.json'
+            r = requests.get(requests_path)
+            r.encoding = 'utf-8'
+            gitpage = r.text
+            soup = BeautifulSoup(gitpage, 'html.parser')
+            main_content = soup.find('td',id = 'LC1').text
+            result = json.loads(main_content)
+        except:
+            result = json.loads("['error':'参数填入有误']")
     return result
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         path = self.path
         path = path.replace("'", '"')
-        user = ''
-        repo = 'friends'
-        branch = 'master'
         repo_reg = re.compile(r'repo="(.*?)"')
         user_reg = re.compile(r'user="(.*?)"')
         branch_reg = re.compile(r'branch="(.*?)"')
         user = user_reg.findall(path)[0]
         repo = repo_reg.findall(path)[0]
         branch = branch_reg.findall(path)[0]
-        data = github_json(user,repo)
+        data = github_json(user,repo，branch)
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type', 'application/json')
